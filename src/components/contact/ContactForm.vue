30b7245b30c721db534050be2f255a4bd37d7761
andreiz53 <andrei.zamfira53@gmail.com>
2023-06-18 17:27:47 +0300

    created contact form

--- src/components/contact/ContactForm.vue
+++ src/components/contact/ContactForm.vue
@@ -1,11 +1,55 @@
+<script setup lang="ts"></script>
+
 <template>
   <div class="form-wrapper">
-    <FormKit type="form">
-      <FormKit type="text" label="label here" name="name" id="name" />
+    <FormKit type="form" submit-label="Submit">
+      <FormKit
+        type="text"
+        placeholder="Name"
+        name="name"
+        id="name"
+        validation="required"
+      />
+      <FormKit
+        type="email"
+        placeholder="Email Address"
+        name="email"
+        id="email"
+        validation="required|email"
+      />
+      <FormKit
+        type="number"
+        placeholder="Phone"
+        id="phone"
+        name="phone"
+        validation="required"
+      />
+      <FormKit
+        type="textarea"
+        placeholder="Your Message"
+        validation="required"
+        name="message"
+        id="message"
+      />
     </FormKit>
   </div>
 </template>
 
-<script setup lang="ts"></script>
-
-<style scoped></style>
+<style>
+.formkit-input:not([type='submit']) {
+  @apply w-full border-0 border-b border-b-white border-opacity-50 bg-transparent text-white font-normal text-base leading-6 py-3 px-4;
+}
+.formkit-input:not([type='submit'])::placeholder {
+  color: white;
+  opacity: 0.5;
+}
+textarea {
+  @apply resize-none;
+}
+.formkit-outer {
+  @apply mb-3;
+}
+.formkit-input[type='submit'] {
+  @apply text-dark bg-white rounded-lg text-base font-medium leading-5 tracking-[1px] py-4 px-12 uppercase mt-6 block ml-auto;
+}
+</style>
