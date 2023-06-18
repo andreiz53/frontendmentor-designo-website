cd066444b6b154782ddeff26d8ce5e8f2dd98df1
andreiz53 <andrei.zamfira53@gmail.com>
2023-06-18 17:36:50 +0300

    contact page finished

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -12,4 +12,18 @@ import ContactRow from '../components/contact/ContactRow.vue';
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.contact-form-section,
+.contact-locations-section {
+  @apply mb-40 relative;
+}
+.contact-locations-section::after {
+  @apply bg-light -z-10 rounded-[300px] rounded-bl-none;
+  content: '';
+  position: absolute;
+  top: 90px;
+  right: 0;
+  width: 70%;
+  aspect-ratio: 1.69 / 1;
+}
+</style>
