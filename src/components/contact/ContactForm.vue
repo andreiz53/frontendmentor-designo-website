2fbff47cdf34c480be6805ce2502cfe5dc089599
andreiz53 <andrei.zamfira53@gmail.com>
2023-06-23 19:07:53 +0300

    form styling

--- src/components/contact/ContactForm.vue
+++ src/components/contact/ContactForm.vue
@@ -40,16 +40,28 @@
   @apply w-full border-0 border-b border-b-white border-opacity-50 bg-transparent text-white font-normal text-base leading-6 py-3 px-4;
 }
 .formkit-input:not([type='submit'])::placeholder {
-  color: white;
-  opacity: 0.5;
+  @apply text-white opacity-50;
 }
-textarea {
+textarea.formkit-input {
   @apply resize-none;
 }
 .formkit-outer {
   @apply mb-3;
 }
 .formkit-input[type='submit'] {
-  @apply text-dark bg-white rounded-lg text-base font-medium leading-5 tracking-[1px] py-4 px-12 uppercase mt-6 block ml-auto;
+  @apply text-dark bg-white rounded-lg text-base font-medium leading-5 tracking-[1px] 
+  py-4 px-12 uppercase mt-6 block ml-auto duration-300;
+}
+.formkit-input[type='submit']:hover {
+  @apply text-primary shadow-2xl;
+}
+.formkit-input:focus-visible {
+  @apply outline-none border-opacity-100;
+}
+.formkit-input:focus-visible::placeholder {
+  @apply opacity-100;
+}
+.formkit-message {
+  @apply italic mt-2 opacity-80 text-sm;
 }
 </style>
